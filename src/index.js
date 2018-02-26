bc3229580803a877b451b910790cde98abf1784e
pinkrefugee <pinkrefugee@gmail.com>
2018-02-26 11:53:30 +0300

    zeros solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var sum = 0;
+  var n = Math.floor (Math.log(number) / Math.log(5));
+  for (var i = 1; i <= n; i++)
+  {
+     sum = sum + Math.floor(number/(Math.pow(5,i)));
+  }
+  return sum;
 }
